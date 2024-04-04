3f81ce79f714a0606464bb0a61ae72770affeef3
tizianomarchetti <27Dicembre16>
2024-04-04 11:08:36 +0200

    fix varie

--- src/app/component/program/program.component.ts
+++ src/app/component/program/program.component.ts
@@ -6,6 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./program.component.css']
 })
 export class ProgramComponent implements OnInit {
+  programCards = {
+    rows: [
+      {
+        margin: 'mt-3',
+        cols: [
+          {
+            class: 'fa-regular fa-clock',
+            innerHtml: 'text.program.lessons'
+          },
+          {
+            class: 'fa-solid fa-users',
+            innerHtml: 'text.program.ensemble'
+          }
+        ]
+      },
+      {
+        margin: 'mb-3',
+        cols: [
+          {
+            class: 'fa-solid fa-music',
+            innerHtml: 'text.program.duo'
+          },
+          {
+            class: 'fa-solid fa-volume-high',
+            innerHtml: 'text.program.concert'
+          }
+        ]
+      }
+    ]
+  };
+
+  priceCards = [
+    {
+      title: 'text.program.master_only',
+      price: '€350',
+      listItems: [
+        'text.program.details.subscription',
+        'text.program.details.accomodation',
+        'text.program.details.breakfast'
+      ]
+    },
+    {
+      title: 'text.program.full_pack',
+      price: '€430',
+      listItems: [
+        'text.program.details.subscription',
+        'text.program.details.accomodation',
+        'text.program.details.breakfast',
+        'text.program.details.lunch',
+        'text.program.details.dinner'
+      ]
+    }
+  ];
 
   constructor() { }
 
